d17ac9c82fd3bc74df4737a7ee2d4cb9d3d94de9
Łukasz Gajowy <lukasz.gajowy@gmail.com>
2019-09-19 13:39:07 +0200

    PoC to prove that with beam 2.14 BEAM-5061 does not occur

--- src/main/java/com/gajowy/beamJava11Compatibility/Main.java
+++ src/main/java/com/gajowy/beamJava11Compatibility/Main.java
@@ -1,31 +1,70 @@
 package com.gajowy.beamJava11Compatibility;
 
-import java.util.Arrays;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.transforms.Count;
-import org.apache.beam.sdk.transforms.Filter;
-import org.apache.beam.sdk.transforms.FlatMapElements;
-import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
+import org.apache.beam.sdk.transforms.join.CoGroupByKey;
+import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
 import org.apache.beam.sdk.values.KV;
-import org.apache.beam.sdk.values.TypeDescriptors;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TupleTag;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
-
   public static void main(String[] args) {
-    PipelineOptions options = PipelineOptionsFactory.fromArgs(args).as(PipelineOptions.class);
-    Pipeline pipeline = Pipeline.create(options);
+    PipelineOptions options = PipelineOptionsFactory.create();
+    Pipeline p = Pipeline.create(options);
 
-    pipeline.apply(TextIO.read().from("wordsToRead.txt"))
-        .apply(FlatMapElements.into(TypeDescriptors.strings())
-            .via((String word) -> Arrays.asList(word.split("[^\\p{L}]+"))))
-        .apply(Filter.by((String word) -> !word.isEmpty())).apply(Count.perElement())
-        .apply(MapElements.into(TypeDescriptors.strings())
-            .via((KV<String, Long> wordCount) -> wordCount.getKey() + ": " + wordCount.getValue()))
-        .apply(TextIO.write().to("wordcounts"));
+    final TupleTag<String> emailsTag = new TupleTag<>();
+    final TupleTag<String> phonesTag = new TupleTag<>();
 
-    pipeline.run().waitUntilFinish();
+    final List<KV<String, String>> emailsList =
+        Arrays.asList(
+            KV.of("amy", "amy@example.com"),
+            KV.of("carl", "carl@example.com"),
+            KV.of("julia", "julia@example.com"),
+            KV.of("carl", "carl@email.com"));
+
+    final List<KV<String, String>> phonesList =
+        Arrays.asList(
+            KV.of("amy", "111-222-3333"),
+            KV.of("james", "222-333-4444"),
+            KV.of("amy", "333-444-5555"),
+            KV.of("carl", "444-555-6666"));
+
+
+    PCollection<KV<String, String>> emails = p.apply("CreateEmails", Create.of(emailsList));
+    PCollection<KV<String, String>> phones = p.apply("CreatePhones", Create.of(phonesList));
+
+    PCollection<KV<String, CoGbkResult>> results =
+        KeyedPCollectionTuple.of(emailsTag, emails)
+            .and(phonesTag, phones)
+            .apply(CoGroupByKey.<String>create());
+
+    PCollection<String> contactLines =
+        results.apply(
+            ParDo.of(
+                new DoFn<KV<String, CoGbkResult>, String>() {
+                  @ProcessElement
+                  public void processElement(ProcessContext c) {
+                    KV<String, CoGbkResult> e = c.element();
+                    String name = e.getKey();
+                    Iterable<String> emailsIter = e.getValue().getAll(emailsTag);
+                    Iterable<String> phonesIter = e.getValue().getAll(phonesTag);
+                    String formattedResult = "";
+                    System.out.println(name);
+                    System.out.println(emailsIter);
+                    System.out.println(phonesIter);
+                    c.output(formattedResult);
+                  }
+                }));
+
+    p.run().waitUntilFinish();
   }
 }
--- src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module com.gajowy.beamJava11Compatibility {
-  exports com.gajowy.beamJava11Compatibility;
-  requires beam.sdks.java.core;
-}
